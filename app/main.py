85cc01c14a338b7ccf946feeec86a0425c9474aa
Karthik Jagadeesan <insufferablejake@gmail.com>
2023-05-26 17:49:51 -0700

    stage 2

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 import socket
+# TODO add simple logging levels
+
+
+def make_simple_string(s: str) -> str:
+    # given a regular string, make it a simple string that follows
+    # the redis protocol
+    return f"+{s}\r\n"
 
 
 def main():
@@ -9,6 +16,12 @@ def main():
     with socket.create_server(('localhost', 6379), reuse_port=True) as server:
         while True:
             conn, addr = server.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                response = make_simple_string("PONG").encode()
+                # sendall() blocks and ties to send all the data you have, whereas send() might not.
+                # apparently less error-prone to use sendall()
+                conn.sendall(response)
 
 
 if __name__ == "__main__":
