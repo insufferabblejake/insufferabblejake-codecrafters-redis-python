167f6c3e7e5b69c820af647b3b0036cb14b3f9a1
Karthik Jagadeesan <insufferablejake@gmail.com>
2023-06-01 22:32:22 -0700

    Stage4: implemented handling multiple connections using select()

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import socket
+import selectors
+import types
 
 HOST = "localhost"
 PORT = 6379
@@ -22,30 +24,79 @@ class RedisServer:
     def __init__(self):
         self.host = HOST
         self.port = PORT
+        self.sel = selectors.DefaultSelector()
         self.connection = None
         self.addr = None
 
+    # Sets up the listening socket, registers it and starts the event loop
+    # the listening socket is only registered for read events
     def start(self):
-        with socket.create_server(('localhost', 6379), reuse_port=True) as server:
-            while True:
-                self.connection, self.addr = server.accept()
-                self.handle_single_connection()
-                self.connection, self.addr = None, None
+        print(f"Starting Redis Server.")
+        listening_sock: socket.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+        listening_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        listening_sock.bind((self.host, self.port))
+        listening_sock.listen()
+        print(f"Listening on {self.host}:{self.port}")
+        listening_sock.setblocking(False)
+        self.sel.register(listening_sock, selectors.EVENT_READ, data=None)
+        self._start_eventloop()
 
-    def handle_single_connection(self):
-        print(f"Connected to by {self.addr[0]}:{self.addr[1]}")
-        with self.connection:
+    def _start_eventloop(self):
+        print(f"Starting event loop ...")
+        try:
             while True:
-                data: bytes = self.connection.recv(1024)
-                if not data:
-                    break
-                command: str = RedisUtils.extract_command(data.decode('utf-8').split('\r\n'))
-                print(f"Received command: {command}")
-                match command:
-                    case "ping":
-                        response: str = RedisUtils.make_redis_simple_string("PONG")
-                        print(f"Sending: {response}")
-                self.connection.sendall(response.encode())
+                events = self.sel.select(timeout=None)
+                for key, mask in events:
+                    if key.data is None:    # new client conn, needs to be registered
+                        self._accept_wrapper(key.fileobj)
+                    else:                   # already registered client, needs to be serviced
+                        self._handle_single_connection(key, mask)
+        except KeyboardInterrupt:
+            print("Exiting on user Ctrl-C")
+        finally:
+            self.sel.close()
+
+    # Each new client socket gets registered to be selected for later when ready
+    def _accept_wrapper(self, sock: object):
+        conn, addr = sock.accept()
+        print(f"Accepted connection from {addr}")
+        conn.setblocking(False)
+        data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        self.sel.register(conn, events, data=data)
+
+    def _handle_single_connection(self, key, mask):
+        connection = key.fileobj
+        data = key.data
+        # print(f"Connected to by {data.addr[0]}:{data.addr[1]}")
+        if mask & selectors.EVENT_READ:
+            self._handle_client_read(connection, data)
+        if mask & selectors.EVENT_WRITE:
+            self._handle_client_write(connection, data)
+
+    def _handle_client_read(self, connection, data):
+        recv_data = connection.recv(1024)
+        if recv_data:
+            command: str = RedisUtils.extract_command(recv_data.decode('utf-8').split('\r\n'))
+            print(f"Received command {command} on {data.addr}")
+            match command:
+                case "ping":
+                    response: str = RedisUtils.make_redis_simple_string("PONG")
+                    print(f"Sending {response} on {data.addr}")
+                    data.outb += response.encode()
+        else:
+            print(f"Closing connection to {data.addr}")
+            self.sel.unregister(connection)
+            connection.close()
+
+    def _handle_client_write(self, connection, data):
+        # you can save data to be written out to the connection when it is ready to be written to
+        # data gets accumulated in outb and each time it is ready to be sent, try a send().
+        # this way we avoid having to call sendall()
+        if data.outb:
+            sent = connection.send(data.outb)
+            # get rid of data already sent
+            data.outb = data.outb[sent:]
 
 
 def main():
